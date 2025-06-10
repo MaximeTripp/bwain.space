4a20de473216ded6d17ef2a5e62efa6ba12f8bf4
Maxime Trippenbach <maxime.trippenbach@gmail.com>
2025-06-10 16:53:33 -0400

    Added dark background to MainLayout and fixed font stylings.

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,18 +3,17 @@ import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
-
 const MainLayout = () => {
   return (
-    <>
+    <div className="bg-gray-50 dark:bg-gray-900 min-h-screen flex flex-col">
+      <NavBar />
 
-        <NavBar/>
-    
-        <Outlet/>
+      <main className="flex-grow">
+        <Outlet />
+      </main>
 
-        <Footer/>
-
-    </>
+      <Footer />
+    </div>
   )
 }
 
--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -5,11 +5,11 @@ const NotFoundPage = () => {
   return (
     <section className='text-center flex flex-col justify-center items-center h-96'>
       <FaExclamationTriangle className='text-yellow-400 text-6xl mb-4' />
-      <h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
-      <p className='text-xl mb-5'>This page does not exist</p>
+      <h1 className='text-gray-500 dark:text-gray-400 text-6xl font-bold mb-4'>404 Not Found</h1>
+      <p className='font-light text-gray-500 dark:text-gray-400 text-xl mb-5'>This page does not exist</p>
       <Link
         to='/'
-        className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
       >
         Go Back
       </Link>
